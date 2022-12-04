11e83a8f0dbfc9c8cbf8f2dc7e2d4b72c5f672ea
Dawsonkw <dawsonkw@gmail.com>
2022-12-04 07:17:48 -0700

    updated darkmode to always on by default, updated color scheme

--- pages/index.js
+++ pages/index.js
@@ -9,10 +9,11 @@ import port2 from "/public/002.png"
 import port3 from "/public/003.png"
 import port4 from "/public/004.png"
 import { useState } from 'react';
+import {theme, useTheme } from "next-themes";
 
 
 export default function Home() {
-  const [darkMode, setDarkMode] = useState(false);
+  const [darkMode, setDarkMode] = useState(true);
   return (
     <div className= {darkMode ? 'dark' : ""}>
       <Head>
@@ -36,7 +37,7 @@ export default function Home() {
           </nav>
           <div className='text-center p-10'>
             <h2 className='text-5xl py-2 text-teal-600 font-medium md:text-6xl'>Dawson Woolley</h2>
-            <h3 className='text-2xl py-2 md:text-3xl'>Frontend developer and designer</h3>
+            <h3 className='text-2xl py-2 md:text-3xl text-gray-500'>Frontend developer and designer</h3>
             <p className='text-md py-5 leading-8 text-gray-500 md:text-xl'>I am a software engineer with a passion for frontend development and design. </p>
             <p className='text-md py-5 leading-8 text-gray-500 md:text-xl'>I am currently seeking fulltime employment</p>
           </div>
@@ -52,7 +53,7 @@ export default function Home() {
 {/* Section 2 */}
         <section>
           <div className='text-center'>
-            <h3 className='text-3xl py-1'>Services I offer</h3>
+            <h3 className='text-3xl py-1 text-teal-600'>Services I offer</h3>
             <p className='text-center text-md py-2 leading-8 text-gray-500 md:text-xl'>
               Since the beginning of my journey as a Frontend Developer, I have had the opportunity to work with many technologies which has led me to building my own personal projects. I am also an expert in Brazilian Jiu Jitsu, currently holding a rank of Brown Belt under the esteemed <a href="https://www.bjjheroes.com/bjj-fighters/amal-easton"><span className='text-teal-600'>Professor Amal Easton</span></a>. 
             </p>
@@ -60,11 +61,11 @@ export default function Home() {
           </div>
           <div className='lg:flex gap-10'>
             <div className='text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white' >
-              <Image className="right-5" src={kitsune} class='object-none object-center m-auto'/>
-              <h3 className='text-lg font-medium pt-8 pb-2'>Beautiful and Responsive Design</h3>
+              <Image src={kitsune} class='object-none object-center m-auto'/>
+              <h3 className='text-lg font-medium pt-8 pb-2 text-teal-600'>Beautiful and Responsive Design</h3>
               <p>Creating elegant and highly responsive designs for whatever project you may have in mind. 
               </p>
-              <h4 className='text-teal-600'>Technologies I work with</h4>
+              <h4 className='text-gray-500'>Technologies I work with</h4>
               <p className='text-gray-500 py-1'>Javascript</p>
               <p className='text-gray-500 py-1'>React</p>
               <p className='text-gray-500 py-1'>HTML / CSS</p>
@@ -74,7 +75,7 @@ export default function Home() {
             </div>
             <div className='text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white place-content-center'>            
             <Image src={kitsune} class='object-none object-center m-auto'/>            
-              <h3 className='text-lg font-medium pt-8 pb-2'>Brazilian Jiu Jitsu Lessons</h3>
+              <h3 className='text-lg font-medium pt-8 pb-2 text-teal-600'>Brazilian Jiu Jitsu Lessons</h3>
               <p>I am available to teach private and small group lessons in Brazilian Jiu Jitsu. 
               </p>
               <h4> Reach out to me to get started or continue training in the fun and challenging sport of Brazilian Jiu Jitsu</h4>
@@ -84,27 +85,27 @@ export default function Home() {
         </section>
         <section>
           <div>
-            <h3 className='text-3xl py-1'>Portfolio</h3>
+            <h3 className='text-3xl py-1 text-teal-600'>Portfolio</h3>
             <div className='flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap'>
               <div className='basis-1/3 flex-1'>
-                <h3 className='text-xl text-teal-600'>JS based To Do List Creator</h3>
+                <h3 className='text-xl text-gray-500'>JS based To Do List Creator</h3>
                 <Image src={port4} className="rounded-lg object-cover"width={'100%'} height={'100%'}/>
-                <a href="https://github.com/Dawsonkw/jsToDoList">Code</a>
+                <a href="https://github.com/Dawsonkw/jsToDoList" className='text-blue-400'>Code</a>
               </div>
               <div className='basis-1/3 flex-1'>
-                <h3 className='text-xl text-teal-600'>Pokedex</h3>
+                <h3 className='text-xl text-gray-500'>Pokedex</h3>
                 <Image src={port2} className="rounded-lg object-cover"width={'100%'} height={'100%'}/>
-                <a href="https://github.com/Dawsonkw/pokedex">Code</a>
+                <a href="https://github.com/Dawsonkw/pokedex" className='text-blue-400'>Code</a>
               </div>
               <div className='basis-1/3 flex-1'>
-                <h3 className='text-xl text-teal-600'>Simple JS CLock App</h3>
+                <h3 className='text-xl text-gray-500'>Simple JS CLock App</h3>
                 <Image src={port1} className="rounded-lg object-cover"width={'100%'} height={'100%'}/>
-                <a href="https://github.com/Dawsonkw/jsClock">Code</a>
+                <a href="https://github.com/Dawsonkw/jsClock" className='text-blue-400'>Code</a>
               </div>
               <div className='basis-1/3 flex-1'>
-                <h3 className='text-xl text-teal-600'>JS Astronomy Picture of the Day App</h3>
+                <h3 className='text-xl text-gray-500'>JS Astronomy Picture of the Day App</h3>
                 <Image src={port3} className="rounded-lg object-cover"width={'100%'} height={'100%'}/>
-                <a href="https://github.com/Dawsonkw/nasaAPODapp">Code</a>
+                <a href="https://github.com/Dawsonkw/nasaAPODapp" className='text-blue-400'>Code</a>
               </div>
             </div>
           </div>
@@ -113,3 +114,12 @@ export default function Home() {
     </div>
   )
 }
+
+
+// TO DO
+// Make your darkmode into a toggle button, here's a good example
+// https://kenaszogara.vercel.app/posts/how-to-create-dark-mode-toggle-button  
+
+
+
+
